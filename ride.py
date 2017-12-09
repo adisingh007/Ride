6367db0891f8381881a264d07478d1348eaab70e
Aditya Singh <aditya.sin@media.net>
2017-12-09 16:11:18 +0530

    Replaced double quotes with single quotes throughout the file.

--- ride.py
+++ ride.py
@@ -8,23 +8,23 @@ import requests
 drop_details = json.load(open('drop-details.json'))
 
 # Getting the session ID
-index_page = "http://drops/index.php"
+index_page = 'http://drops/index.php'
 index_page_response = requests.get(index_page)
 cookies = index_page_response.headers['Set-Cookie'].split(';')  # Getting the session ID
-session_id = ""
+session_id = ''
 for cookie in cookies:
     if cookie.startswith('PHPSESSID'):
         session_id = cookie
         break
 
-if session_id != "":
+if session_id != '':  # A session has been created
     # Creating a header dictionary that will be used throughout the app.
     headers = {
         'Cookie': session_id
     }
 
     # Logging in
-    login_page = "http://drops/login.php"
+    login_page = 'http://drops/login.php'
     login_params = {
         'src': drop_details['src'],  # 1: Mumbai-Plex, 2: Mumbai-Seepz
         'uname': drop_details['uname'],
@@ -34,11 +34,11 @@ if session_id != "":
 
     if login_response.text == '1':  # Successful login
         # Getting the home page
-        home_page = "http://drops/home.php"
+        home_page = 'http://drops/home.php'
         home_page_response = requests.get(home_page, headers=headers)
 
         # Get location ID
-        location_url = "http://172.16.185.58:5001/locations?filter=enabled"
+        location_url = 'http://172.16.185.58:5001/locations?filter=enabled'
         locations = requests.get(location_url, headers=headers)
         locations_text = json.loads(locations.text)
         my_location_id = -1
@@ -48,7 +48,7 @@ if session_id != "":
                 break
 
         # Get timings list
-        timings_url = "http://172.16.185.58:5001/time?filter=available&sid=" + str(drop_details['src'])
+        timings_url = 'http://172.16.185.58:5001/time?filter=available&sid=' + str(drop_details['src'])
         timings = requests.get(timings_url, headers=headers)
         timings_text = json.loads(timings.text)
         tid = -1
@@ -62,7 +62,7 @@ if session_id != "":
                 break
 
         # Book drop finally
-        booking_url = "http://drops/setdrop.php"
+        booking_url = 'http://drops/setdrop.php'
         booking_params = {
             'lid': my_location_id,
             'tid': tid,
